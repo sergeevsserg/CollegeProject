d913c3d106171dffa3d0f07c8109fb0f14016f4c
sergeevsserg <sergeevserg160707@gmail.com>
2023-10-14 22:24:19 +0700

    бета - версия выстрелов

--- entity.py
+++ entity.py
@@ -4,7 +4,7 @@ import pygame
 class Enemy:
     def __init__(self, model, speed, spawn_rate, hp, damage, x, y, height, width):
         self.model = pygame.transform.scale(pygame.image.load(model), [height, width])
-        self.revers_model = pygame.transform.flip(pygame.transform.scale(pygame.image.load(model), [height, width] ), 180, 0)
+        self.revers_model = pygame.transform.flip(pygame.transform.scale(pygame.image.load(model), [height, width]), 1, 0)
         self.speed = speed
         self.spawn_rate = spawn_rate
         self.hp = hp
@@ -18,9 +18,9 @@ class Enemy:
     def moving_enemy(enemy, player, win):
         for i in range(len(enemy)):
             rot = False
-            if enemy[i].x > player.x + player.height/4:
+            if enemy[i].x > player.x + player.height/8:
                 enemy[i].x -= enemy[i].speed
-            if enemy[i].x < player.x + player.height/4:
+            if enemy[i].x < player.x + player.height/8:
                 enemy[i].x += enemy[i].speed
                 rot = True
             if enemy[i].y > player.y + player.width/4:
@@ -48,3 +48,25 @@ class Enemy:
             win.blit(self.revers_model, [self.x, self.y])
         else:
             win.blit(self.model, [self.x, self.y])
+
+    def touch_kill(self, player):
+        kill_list = []
+        for i in range(len(self)):
+            if player.x - player.height/2 <= self[i].x <= player.x + player.height/2 and player.y - player.width/2 <= self[i].y <= player.y + player.width/1.5:
+                kill_list.append(i)
+        for j in kill_list:
+            self.pop(j)
+
+
+    def shooting(self, enemy, win):
+        for i in range(len(self)):
+            if self[i].x > enemy.x + enemy.height:
+                self[i].x -= self[i].speed
+            if self[i].x < enemy.x + enemy.height:
+                self[i].x += self[i].speed
+                rot = True
+            if self[i].y > enemy.y + enemy.width:
+                self[i].y -= self[i].speed
+            if self[i].y < enemy.y + enemy.width:
+                self[i].y += self[i].speed
+            win.blit(self[i].model, [self[i].x, self[i].y])
