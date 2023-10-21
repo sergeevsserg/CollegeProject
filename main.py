1444fb494a6c24aa8a79c4f3618e8fe4b0a63d30
tyulinSV <Sergeevserg160707@gmail.com>
2023-10-21 14:12:53 +0700

    доделать экран смерти

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def anmation():
 
 
 pygame.get_init()
+pygame.font.init()
 win = pygame.display.set_mode((1000, 1000), )
 pygame.display.set_caption('FOXYSHMERTS.INC')
 
@@ -25,52 +26,72 @@ player = entity.Enemy("pictures/pers.png", 3, 0, 100, 5, 440, 350, 70, 120)
 main_title = entity.Menu('pictures/title.png', 210, 200, 600, 80)
 start_button = entity.Menu('pictures/start_button.png', 340, 500, 300, 100)
 exit_button = entity.Menu('pictures/exit_button.png', 900, 0, 100, 100)
+lose_title1 = entity.Menu('pictures/lose_text1.png', 210, 200, 600, 80)
+lose_title2 = entity.Menu('pictures/lose_text_2.png', 400, 400, 600, 80)
 
 hams = []
 bullets = []
 start_time = time.time()
 x = 0
 y = 0
+f1 = pygame.font.Font(None, 36)
 
 run = False
 menu = True
-while menu:
-    for event in pygame.event.get():
-        win.blit(image, (0, 0))
-        win.blit(player.model, [player.x, player.y])
-        win.blit(main_title.picture, (main_title.x, main_title.y))
-        win.blit(start_button.picture, (start_button.x, start_button.y))
-        win.blit(exit_button.picture, (exit_button.x, exit_button.y))
-        pygame.display.update()
-        if event.type == pygame.QUIT:
-            menu = False
-        if event.type == pygame.MOUSEMOTION:
-            x, y = event.pos
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if start_button.x <= x <= start_button.x + start_button.height and start_button.y <= y <= start_button.y + start_button.wigth:  # Кнопка старта
+Game = True
+while Game:
+    while menu:
+        for event in pygame.event.get():
+            win.blit(image, (0, 0))
+            win.blit(player.model, [player.x, player.y])
+            win.blit(main_title.picture, (main_title.x, main_title.y))
+            win.blit(start_button.picture, (start_button.x, start_button.y))
+            win.blit(exit_button.picture, (exit_button.x, exit_button.y))
+            pygame.display.update()
+            if event.type == pygame.QUIT:
                 menu = False
-                run = True
-                anmation()
-            if exit_button.x <= x <= exit_button.x + exit_button.height and exit_button.y <= y <= exit_button.y + exit_button.wigth:  # Кнопка выхода
-                menu = False
-while run:
-    pygame.time.delay(20)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+                Game = False
+            if event.type == pygame.MOUSEMOTION:
+                x, y = event.pos
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_button.x <= x <= start_button.x + start_button.height and start_button.y <= y <= start_button.y + start_button.wigth:  # Кнопка старта
+                    menu = False
+                    run = True
+                    anmation()
+                if exit_button.x <= x <= exit_button.x + exit_button.height and exit_button.y <= y <= exit_button.y + exit_button.wigth:  # Кнопка выхода
+                    menu = False
+                    Game = False
+    while run:
+        pygame.time.delay(20)
+        text = f1.render('HP: ', 1, (255, 0 ,0))
+        text1 = f1.render(str(player.hp), 1, (255, 0 ,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                Game = False
+        if player.hp <= 0:
             run = False
-    win.blit(image, (0, 0))
-    current_time = time.time()
-    entity.Enemy.moving_enemy(hams, player, win)
-    entity.Enemy.moving_player(player, win)
-    entity.Enemy.touch_kill(hams, player, bullets)
-    if len(hams) != 0:
-        entity.Enemy.shooting(bullets, hams[0], win)
-    if current_time - start_time >= 2:
-        for i in range(2):
-            hams.append(entity.Enemy("pictures/hames.png", 1, 10, 5, 1, randint(0, 1000),
-                                 randint(0, 1000), 100, 60))
-        start_time = current_time
-        bullets.append(entity.Enemy('pictures/bullet.png', 5, 0, 0, 10, player.x, player.y, 10, 10))
-    pygame.display.update()
+            menu = True
+            hams.clear()
+            bullets.clear()
+            player.hp = 100
+            player.x = 440
+            player.y = 350
+        win.blit(image, (0, 0))
+        current_time = time.time()
+        entity.Enemy.moving_enemy(hams, player, win)
+        entity.Enemy.moving_player(player, win)
+        entity.Enemy.touch_kill(hams, player, bullets)
+        win.blit(text, (0, 0))
+        win.blit(text1, (40, 0))
+        if len(hams) != 0:
+            entity.Enemy.shooting(bullets, hams[0], win)
+        if current_time - start_time >= 2:
+            for i in range(2):
+                hams.append(entity.Enemy("pictures/hames.png", 1, 10, 5, 5, randint(0, 1000),
+                                     randint(0, 1000), 100, 60))
+            start_time = current_time
+            bullets.append(entity.Enemy('pictures/bullet.png', 10, 0, 0, 4, player.x + player.height / 2, player.y + player.width / 2, 20, 20))
+        pygame.display.update()
 
 pygame.quit()
