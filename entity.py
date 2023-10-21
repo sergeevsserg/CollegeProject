6cd17fb0e83c0c1fc61a8fc65430ab9e62419d9f
tyulinSV <Sergeevserg160707@gmail.com>
2023-10-21 14:13:13 +0700

    создана система урона

--- entity.py
+++ entity.py
@@ -50,15 +50,20 @@ class Enemy:
         kill_list2 = []
         for i in range(len(self)):
             if player.x - player.height/2 <= self[i].x <= player.x + player.height/2 and player.y - player.width/2.5 <= self[i].y <= player.y + player.width/1.2:
+                player.hp -= self[i].damage
                 kill_list.append(i)
             for j in range(len(bullet)):
                 if bullet[j].x - bullet[j].height <= self[i].x + self[i].height / 2 <= bullet[j].x + bullet[j].height and bullet[j].y - bullet[j].width <= self[i].y + self[i].width / 2 <= bullet[j].y + bullet[j].width:
-                    kill_list.append(i)
+                    self[i].hp -= bullet[j].damage
                     kill_list2.append(j)
+            if self[i].hp <= 0:
+                kill_list.append(i)
         for b in kill_list:
-            self.pop(b)
+            if b < len(self):
+                self.pop(b)
         for u in kill_list2:
-            bullet.pop(u)
+            if u < len(bullet):
+                bullet.pop(u)
 
 
     def shooting(self, enemy, win):
