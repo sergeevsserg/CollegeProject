eba64aa8840d910587d0676360eca8542fa23073
TyulinSV <segeevserg160707@gmail.com>
2023-11-24 16:09:28 +0700

    добавленны некоторые карточки улучшения

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from random import randint
 import math
 import threading
@@ -12,7 +13,7 @@ class Menu:
         self.picture = pygame.transform.scale(pygame.image.load(picture), (height, width))
 
 class Enemy:
-    def __init__(self, model, speed, spawn_rate, hp, damage, x, y, height, width, lvl = None):
+    def __init__(self, model, speed, spawn_rate, hp, damage, x, y, height, width):
         self.model = pygame.transform.scale(pygame.image.load(model), [height, width])
         self.revers_model = pygame.transform.flip(pygame.transform.scale(pygame.image.load(model), [height, width]), 1, 0)
         self.speed = speed
@@ -23,27 +24,28 @@ class Enemy:
         self.y = y
         self.height = height
         self.width = width
-        self.lvl = lvl
 
-def lvl():
-    global xp, xp_need
-    key = 20
-    pygame.draw.rect(win, (42, 219, 219), (0, 0, 1000 * xp/xp_need, 20))
-    if xp >= xp_need:
-        xp = 0
-        xp_need += key * 2
-        key = key * 2
-
+class upgrade_card:
+    def __init__(self, name, tier, description, type, damage, cd, speed, hp, prot):
+        self.name = name
+        self.tier = tier
+        self.description = description
+        self.type = type
+        self.damage = damage
+        self.cd = cd
+        self.speed = speed
+        self.hp = hp
+        self.prot = prot
 def hard():
     global hard_k
     while working:
         while get_harder:
-            hard_k += 0.2
+            hard_k += 0.5
             pygame.time.delay(30000)
 def shoot():
     while working:
         while is_shooting:
-            bullets.append(Enemy('pictures/bullet.png', 10, 1, 0, 4, player.x + player.height / 2, player.y + player.width / 2, 40, 40))
+            bullets.append(Enemy('pictures/bullet.png', 10, 1, 0, 4, player.x + player.height / 2, player.y + player.width / 2, 20, 20))
             pygame.time.delay(bullets[-0].spawn_rate * 1000)
 def spawn():
     while working:
@@ -67,6 +69,13 @@ def moving_player():
         win.blit(player.revers_model, [player.x, player.y])
     else:
         win.blit(player.model, [player.x, player.y])
+
+def xp():
+    global current_xp, xp_need
+    pygame.draw.rect(win, (37, 245, 165), (0,0, 1000 * current_xp / xp_need, 20))
+    if xp_need <= current_xp:
+        current_xp = 0
+        xp_need *= 2
 
 def shooting():
     for i in range(len(bullets)):
@@ -100,7 +109,7 @@ def anmation():
         pygame.time.delay(1)
 
 def touch_kill():
-    global xp
+    global current_xp
     kill_list = []
     kill_list2 = []
     for i in range(len(hams)):
@@ -108,15 +117,15 @@ def touch_kill():
             player.hp -= hams[i].damage
             kill_list.append(i)
         for j in range(len(bullets)):
-            if bullets[j].x - bullets[j].height / 2 <= hams[i].x + hams[i].height / 2 <= bullets[j].x + bullets[j].height / 2 and bullets[j].y - bullets[j].width / 2 <= hams[i].y + hams[i].width / 2 <= bullets[j].y + bullets[j].width / 2:
+            if bullets[j].x - bullets[j].height <= hams[i].x + hams[i].height / 2 <= bullets[j].x + bullets[j].height and bullets[j].y - bullets[j].width <= hams[i].y + hams[i].width / 2 <= bullets[j].y + bullets[j].width:
                 hams[i].hp -= bullets[j].damage
                 kill_list2.append(j)
         if hams[i].hp <= 0:
             kill_list.append(i)
-            xp += 20 * hard_k
     for b in kill_list:
         if b < len(hams):
             hams.pop(b)
+            current_xp += 20
     for u in kill_list2:
         if u < len(bullets):
             bullets.pop(u)
@@ -131,19 +140,34 @@ win = pygame.display.set_mode((1000, 1000), )
 pygame.display.set_caption('FOXYSHMERTS.INC')
 
 image = pygame.image.load("pictures/bg.jpg")
-player = Enemy("pictures/pers.png", 3, 0, 100, 5, 440, 350, 70, 120, 1)
+player = Enemy("pictures/pers.png", 3, 0, 100, 5, 440, 350, 70, 120)
 main_title = Menu('pictures/title.png', 210, 200, 600, 80)
 start_button = Menu('pictures/start_button.png', 340, 500, 300, 100)
 exit_button = Menu('pictures/exit_button.png', 900, 0, 100, 100)
 lose_title1 = Menu('pictures/lose_text1.png', 210, 200, 600, 80)
 lose_title2 = Menu('pictures/lose_text_2.png', 400, 400, 600, 80)
 
-hams = [Enemy("pictures/hames.png", 1, 10, 5, 5, randint(0, 1000), randint(0, 1000),  100, 60)]
+upgrades  = [upgrade_card("Большой калибр", 1, "Большие пушки - большой калибр. Увеличивает урон, увеличивает перезардку", 'up_p', 5, -2, 0, 0, 0),
+             upgrade_card("Миник", 1, "Выпей миник, немного повысь лимит здоровья", 'up_p', 0, 0, 0, 25, 0),
+             upgrade_card("Адская плеть", 1,"Раскрутите адскую плеть, наносящую средний урон", 'new_w', 0, 0, 0, 0, 0),
+             upgrade_card("Усовершенствованный затвор", 1, "Установите улучшеный затвор, увеличив тем самым скорострельнсть, немного уменьшая урон", 'up_p', -2, 2, 0, 0, 0),
+             upgrade_card("Бронепластина", 1, "Установите дополнительную бронепластину, уменьшающую входящий урон", 'up_p', 0, 0, 0, 0, 5),
+             upgrade_card("Липкое масло", 1, "Залейте пол липким маслом, замедляющим врагов", 'Weakness', 0, 0, -3, 0, 0),
+             upgrade_card("Паровые тяги", 2, "Уфффф что за тяги такие, паровые, кэфтэме. Увеличивают вашу скорость", 'up_p', 0, 0, 5, 0, 0),
+             upgrade_card("Бига", 2, "Бахни бигу, получи ощутимый прирост к здоровью", 'up_p', 0, 0, 0, 75, 0),
+             upgrade_card("Квантовый щит", 2, "С помощью новейших технологий ощутимо увеличте свою броню", 'up_p', 0, 0, 0 , 0, 12),
+             upgrade_card("Заряженное копьё", 2, "Бросьте медленное копьё, наносящее высокий урон", 'new_w', 0, 0, 0, 0, 0),
+             upgrade_card("Калибр бабахи", 2, "Самые огромные фугасы, но и перезарядка тоже огромная. Мощная прибавка к урону и перезарядке", 'up_p', 20, -8, 0, 0, 0),
+             upgrade_card("Пушка от A10 Thunderbolt", 2, "Тот самый БРРРРРРРРРРРРРРРРРР!!!! Стоп, а где урон?! Огромный прирост к скорострельности, но про урон забудьте", 'up_p', -40, 20, 0, 0, 0),
+             upgrade_card("Услуги дезинсектора", 2, "Протравите своих врагов, уменьшив их наносимый урон")]
+
+hams = Enemy("pictures/hames.png", 1, 10, 5, 5, randint(0, 1000), randint(0, 1000),  100, 60)
 bullets = []
-hard_k = 0.8
+start_time = time.time()
+hard_k = 0.5
 x = 0
 y = 0
-xp = 0
+current_xp = 0
 xp_need = 100
 f1 = pygame.font.Font(None, 36)
 
@@ -205,15 +229,18 @@ while Game:
             menu = True
             hams.clear()
             bullets.clear()
+            xp_need = 100
+            current_xp = 0
             player.hp = 100
             player.x = 440
             player.y = 350
             hard_k = 1
         win.blit(image, (0, 0))
+        current_time = time.time()
         moving_enemy()
         moving_player()
         touch_kill()
-        lvl()
+        xp()
         win.blit(text, (0, 0))
         win.blit(text1, (40, 0))
         shooting()
