d1bc2e7bd78448970d866f2efa3fda7546203fc5
sergeevsserg <sergeevserg160707@gmail.com>
2024-03-21 21:05:17 +0700

    .

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import time
 from random import randint
 import math
 import threading
@@ -7,7 +6,7 @@ import sys
 
 
 class Menu:
-    def __init__(self, picture, x, y, height, width, ):
+    def __init__(self, picture, x, y, height, width):
         self.width = width
         self.height = height
         self.y = y
@@ -48,7 +47,7 @@ def hard():
     while working:
         while get_harder:
             hard_k += 0.5
-            pygame.time.delay(40000)
+            pygame.time.delay(60000)
 
 
 def shoot():
@@ -72,12 +71,12 @@ def moving_player():
     rot = False
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a] and player.x > 0:
-        player.x -= player.speed - buff[2]
+        player.x -= player.speed + buff[2]
         rot = True
     if keys[pygame.K_d] and player.x < 880:
         player.x += player.speed + buff[2]
     if keys[pygame.K_w] and player.y > 0:
-        player.y -= player.speed - buff[2]
+        player.y -= player.speed + buff[2]
     if keys[pygame.K_s] and player.y < 900:
         player.y += player.speed + buff[2]
     if rot:
@@ -91,7 +90,7 @@ def xp():
     pygame.draw.rect(win, (37, 245, 165), (0, 0, 1000 * current_xp / xp_need, 20))
     if xp_need <= current_xp:
         current_xp = 0
-        xp_need *= 2
+        xp_need *= 1.2
         choice = True
         Pause = True
         is_shooting = False
@@ -100,14 +99,23 @@ def xp():
 
 
 def projectile_flight():
-    for i in range(len(bullets)):
-        if len(hams) > 0:
-            dx = hams[0].x - bullets[i].x + hams[0].height / 2
-            dy = hams[0].y - bullets[i].y + hams[0].width / 2
+    if bossfight:
+        for i in range(len(bullets)):
+            dx = boss.x - bullets[i].x + boss.height / 2
+            dy = boss.y - bullets[i].y + boss.width / 2
             distanse = math.sqrt(dx * dx + dy * dy)
             bullets[i].x += bullets[i].speed * dx / distanse
             bullets[i].y += bullets[i].speed * dy / distanse
             win.blit(bullets[i].model, [bullets[i].x, bullets[i].y])
+    else:
+        for i in range(len(bullets)):
+            if len(hams) > 0:
+                dx = hams[0].x - bullets[i].x + hams[0].height / 2
+                dy = hams[0].y - bullets[i].y + hams[0].width / 2
+                distanse = math.sqrt(dx * dx + dy * dy)
+                bullets[i].x += bullets[i].speed * dx / distanse
+                bullets[i].y += bullets[i].speed * dy / distanse
+                win.blit(bullets[i].model, [bullets[i].x, bullets[i].y])
 
 
 def moving_enemy():
@@ -125,9 +133,10 @@ def moving_enemy():
 
 def animation():
     for count in range(50):
-        win.blit(image, (0, 0))
+        win.blit(bg, (0, 0))
         win.blit(player.model, [player.x, player.y])
         win.blit(main_title.picture, (main_title.x, main_title.y - count * 10))
+        win.blit(location_button.picture, (location_button.x, location_button.y + count * 10))
         win.blit(start_button.picture, (start_button.x, start_button.y + count * 10))
         win.blit(exit_button.picture, (exit_button.x + count * 10, 0))
         pygame.display.update()
@@ -135,32 +144,61 @@ def animation():
 
 
 def touch_kill():
+    global Win
+    global bossfight
     global current_xp
+    global score
     kill_list = []
     kill_list2 = []
-    for i in range(len(hams)):
-        if player.x - player.height / 2 <= hams[i].x <= player.x + player.height / 2 and player.y - player.width / 2.5 <= hams[i].y <= player.y + player.width / 1.2:
-            if hams[i].damage - buff[3] - debuff[0] > 0:
-                player.hp -= hams[i].damage - buff[3] - debuff[0]
+    if bossfight:
+        if player.x - player.height / 2 <= boss.x <= player.x + player.height / 2 and player.y - player.width / 2.5 <= boss.y <= player.y + player.width / 1.2:
+            if boss.damage - buff[3] - debuff[0] > 0:
+                player.hp -= boss.damage - buff[3] - debuff[0]
             else:
                 player.hp -= 1
-            kill_list.append(i)
         for j in range(len(bullets)):
-            if bullets[j].x - bullets[j].height <= hams[i].x + hams[i].height / 2 <= bullets[j].x + bullets[j].height and bullets[j].y - bullets[j].width <= hams[i].y + hams[i].width / 2 <= bullets[j].y + bullets[j].width:
+            if (bullets[j].x - bullets[j].height <= boss.x + boss.height / 2 <= bullets[j].x + bullets[j].height and
+                    bullets[j].y - bullets[j].width <= boss.y + boss.width / 2 <= bullets[j].y + bullets[j].width):
                 if bullets[j].damage + buff[0] > 0:
-                    hams[i].hp -= bullets[j].damage + buff[0]
+                    boss.hp -= bullets[j].damage + buff[0]
                 else:
-                    hams[i].hp -= 0.1
-                kill_list2.append(j)
-        if hams[i].hp <= 0:
-            kill_list.append(i)
-    for b in kill_list:
-        if b < len(hams):
-            hams.pop(b)
-            current_xp += 20
-    for u in kill_list2:
-        if u < len(bullets):
-            bullets.pop(u)
+                    boss.hp -= 0.2
+                    kill_list2.append(j)
+            if boss.hp <= 0:
+                bossfight = False
+                Win = True
+        for u in kill_list2:
+            if u < len(bullets):
+                bullets.pop(u)
+    else:
+        for i in range(len(hams)):
+            if player.x - player.height / 2 <= hams[
+                    i].x <= player.x + player.height / 2 and player.y - player.width / 2.5 <= hams[
+                        i].y <= player.y + player.width / 1.2:
+                if hams[i].damage - buff[3] - debuff[0] > 0:
+                    player.hp -= hams[i].damage - buff[3] - debuff[0]
+                else:
+                    player.hp -= 1
+                kill_list.append(i)
+            for j in range(len(bullets)):
+                if bullets[j].x - bullets[j].height <= hams[i].x + hams[i].height / 2 <= bullets[j].x + bullets[
+                    j].height and bullets[j].y - bullets[j].width <= hams[i].y + hams[i].width / 2 <= bullets[j].y + \
+                        bullets[j].width:
+                    if bullets[j].damage + buff[0] > 0:
+                        hams[i].hp -= bullets[j].damage + buff[0]
+                    else:
+                        hams[i].hp -= 0.2
+                    kill_list2.append(j)
+            if hams[i].hp <= 0:
+                kill_list.append(i)
+        for b in kill_list:
+            if b < len(hams):
+                hams.pop(b)
+                current_xp += 20
+                score += 1
+        for u in kill_list2:
+            if u < len(bullets):
+                bullets.pop(u)
 
 
 def level_up():
@@ -203,6 +241,7 @@ def level_up():
                     is_shooting = True
         pygame.time.delay(30)
 
+
 def up(a):
     if upgrades[a].type == 'up_p':
         buff[0] += upgrades[a].damage
@@ -213,6 +252,18 @@ def up(a):
     else:
         debuff[0] += upgrades[a].damage
         debuff[1] += upgrades[a].speed
+
+
+def boss_moving():
+    dx = player.x - boss.x
+    dy = player.y - boss.y
+    distanse = math.sqrt(dx * dx + dy * dy)
+    boss.x += boss.speed * dx / distanse
+    boss.y += boss.speed * dy / distanse
+    if dx > 0:
+        win.blit(boss.revers_model, [boss.x, boss.y])
+    else:
+        win.blit(boss.model, [boss.x, boss.y])
 
 
 shot = threading.Thread(target=shoot)
@@ -227,24 +278,33 @@ pygame.font.init()
 win = pygame.display.set_mode((1000, 1000), )
 pygame.display.set_caption('FOXYSHMERTS.INC')
 
-image = pygame.image.load("pictures/bg.jpg")
+locations = [pygame.image.load("pictures/bg.jpg"),
+             pygame.transform.scale(pygame.image.load("pictures/pesok.jpg"), (1000, 1000)),
+             pygame.transform.scale(pygame.image.load("pictures/iron.jpg"), (1000, 1000))]
+
+image_forest = pygame.image.load("pictures/bg.jpg")
+image_desert = pygame.transform.scale(pygame.image.load("pictures/pesok.jpg"), (1000, 1000))
+image_factory = pygame.transform.scale(pygame.image.load("pictures/iron.jpg"), (1000, 1000))
+
+boss = Enemy("pictures/roboboss.png", 0.8, 0, 1000, 20, 500, 500, 360, 240)
 player = Enemy("pictures/pers.png", 3, 0, 100, 5, 440, 350, 70, 120)
 main_title = Menu('pictures/title.png', 210, 200, 600, 80)
 start_button = Menu('pictures/start_button.png', 340, 500, 300, 100)
 exit_button = Menu('pictures/exit_button.png', 900, 0, 100, 100)
 lose_title1 = Menu('pictures/lose_text1.png', 210, 200, 600, 80)
 lose_title2 = Menu('pictures/lose_text_2.png', 400, 400, 600, 80)
+location_button = Menu('pictures/location_button.png', 340, 600, 300, 100)
 
 upgrades = [upgrade_card("Большой калибр", 1, "pictures/up_cards/big_cal.png", 'up_p', 5, 2, 0, 0, 0),
             upgrade_card("Миник", 1, "pictures/up_cards/minic.png", 'up_p', 0, 0, 0, 25, 0),
             upgrade_card("Бронепластина", 1, "pictures/up_cards/armorplate.png", 'up_p', 0, 0, 0, 0, 2),
-            upgrade_card("Усовершенствованный затвор", 1, "pictures/up_cards/b_shutter.png", 'up_p', -2, -2, 0, 0, 0),
+            upgrade_card("Усовершенствованный затвор", 1, "pictures/up_cards/b_shutter.png", 'up_p', -1, -2, 0, 0, 0),
             upgrade_card("Липкое масло", 1, "pictures/up_cards/sticky_oil.png", 'weakness', 0, 0, -2, 0, 0),
             upgrade_card("Паровые тяги", 2, 'pictures/up_cards/steam_tygi.png', 'up_p', 0, 0, 5, 0, 0),
             upgrade_card("Бига", 2, "pictures/up_cards/biga.png", 'up_p', 0, 0, 0, 75, 0),
             upgrade_card('Квантовый щит', 2, "pictures/up_cards/quantum_shield.png", 'up_p', 0, 0, 0, 0, 8),
-            upgrade_card("Калибр бабахи", 3, "pictures/up_cards/babaha.png", 'up_p', 20, 8, 0, 0, 0),
-            upgrade_card("Пушка от A10 Thunderbolt", 3, "pictures/up_cards/a10.png", 'up_p', -40, -20, 0, 0, 0)]
+            upgrade_card("Калибр бабахи", 3, "pictures/up_cards/babaha.png", 'up_p', 20, 10, 0, 0, 0),
+            upgrade_card("Пушка от A10 Thunderbolt", 3, "pictures/up_cards/a10.png", 'up_p', -40, -15, 0, 0, 0)]
 
 # upgrades  = [upgrade_card("Большой калибр", 1, "Большие пушки - большой калибр. Увеличивает урон, увеличивает перезардку", 'up_p', 5, -2, 0, 0, 0),
 #    upgrade_card("Миник", 1, "Выпей миник, немного повысь лимит здоровья", 'up_p', 0, 0, 0, 25, 0),
@@ -262,19 +322,27 @@ upgrades = [upgrade_card("Большой калибр", 1, "pictures/up_cards/bi
 
 hams = [Enemy("pictures/hames.png", 1, 10, 5, 5, randint(0, 1000), randint(0, 1000), 100, 60)]
 bullets = []
-start_time = time.time()
 hard_k = 0.5
 x = 0
 y = 0
+loc = 0
 current_xp = 0
 xp_need = 100
 f1 = pygame.font.Font(None, 36)
+loc_text = f1.render('', 1, (255, 0, 0))
+fight_text = f1.render('ВНИМАНИЕ!!! Вы разбудиили босса, у него 1000хп и большой контактный урон', 1, (255, 0, 0))
+score = 0
 
+bg = locations[loc]
+
+Win = False
+bossfight = False
 is_spawning = False
 get_harder = False
 working = True
 is_shooting = False
 run = False
+location = False
 death = False
 menu = True
 Game = True
@@ -287,11 +355,13 @@ hd.start()
 while Game:
     while menu:
         for event in pygame.event.get():
-            win.blit(image, (0, 0))
+            win.blit(bg, (0, 0))
             win.blit(player.model, [player.x, player.y])
             win.blit(main_title.picture, (main_title.x, main_title.y))
+            win.blit(location_button.picture, (location_button.x, location_button.y))
             win.blit(start_button.picture, (start_button.x, start_button.y))
             win.blit(exit_button.picture, (exit_button.x, exit_button.y))
+            win.blit(loc_text, (300, 700))
             pygame.display.update()
             if event.type == pygame.QUIT:
                 working = False
@@ -300,14 +370,25 @@ while Game:
             if event.type == pygame.MOUSEMOTION:
                 x, y = event.pos
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_button.x <= x <= start_button.x + start_button.height and start_button.y <= y <= start_button.y + start_button.width:  # Кнопка старта
+                if start_button.x <= x <= start_button.x + start_button.height and start_button.y <= y <= start_button.y + start_button.width:
                     menu = False
                     run = True
                     animation()
-                if exit_button.x <= x <= exit_button.x + exit_button.height and exit_button.y <= y <= exit_button.y + exit_button.width:  # Кнопка выхода
+                if exit_button.x <= x <= exit_button.x + exit_button.height and exit_button.y <= y <= exit_button.y + exit_button.width:
                     menu = False
                     Game = False
                     working = False
+                if location_button.x <= x <= location_button.x + location_button.height and location_button.y <= y <= location_button.y + location_button.width:
+                    loc += 1
+                    if loc + 1 > len(locations):
+                        loc = 0
+                    bg = locations[loc]
+            if loc == 0:
+                loc_text = f1.render('Обычная локация без усложнений', 1, (255, 0, 0))
+            if loc == 1:
+                loc_text = f1.render('+10% к скорости врагов, +10% к урону по врагам', 1, (255, 0, 0))
+            if loc == 2:
+                loc_text = f1.render('+10% к хп врагов, +10% к урону врагов, +50% к опыту', 1, (255, 0, 0))
 
     while run:
         is_shooting = True
@@ -316,6 +397,8 @@ while Game:
         pygame.time.delay(20)
         text = f1.render('HP: ', 1, (255, 0, 0))
         text1 = f1.render(str(player.hp), 1, (255, 0, 0))
+        text2 = f1.render("Счёт: ", 1, (255, 0, 0))
+        text3 = f1.render(str(score), 1, (255, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 working = False
@@ -335,16 +418,61 @@ while Game:
             player.x = 440
             player.y = 350
             hard_k = 1
-        win.blit(image, (0, 0))
+        win.blit(bg, (0, 0))
+        if score >= 150:
+            run = False
+            bossfight = True
+            is_spawning = False
+            hams.clear()
+            bullets.clear()
         if not Pause:
             moving_enemy()
             moving_player()
             touch_kill()
             xp()
+
         win.blit(text, (0, 0))
         win.blit(text1, (40, 0))
+        win.blit(text2, (900, 0))
+        win.blit(text3, (962, 0))
         projectile_flight()
         pygame.display.update()
+
+    while bossfight:
+        text = f1.render('HP: ', 1, (255, 0, 0))
+        text1 = f1.render(str(player.hp), 1, (255, 0, 0))
+        text2 = f1.render("Счёт: ", 1, (255, 0, 0))
+        text3 = f1.render(str(score), 1, (255, 0, 0))
+        win.blit(bg, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                working = False
+                run = False
+                Game = False
+                bossfight = False
+        if player.hp <= 0:
+            is_shooting = False
+            is_spawning = False
+            get_harder = False
+            bossfight = False
+            menu = True
+            hams.clear()
+            bullets.clear()
+            xp_need = 100
+            current_xp = 0
+            player.hp = 100
+            player.x = 440
+            player.y = 350
+            hard_k = 1
+        win.blit(text, (0, 0))
+        win.blit(text1, (40, 0))
+        win.blit(text2, (900, 0))
+        win.blit(text3, (962, 0))
+        moving_player()
+        boss_moving()
+        projectile_flight()
+        pygame.display.update()
+        pygame.time.delay(10)
 
 pygame.quit()
 sys.exit()
